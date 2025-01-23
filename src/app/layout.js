2dac4489476552846840372059d63ddd6528c4e2
Your Correct Username <your_correct_email@example.com>
2025-01-23 15:58:59 +0000

    project done

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Banner from "@/components/Banner";
+
 
 
 const geistSans = Geist({
